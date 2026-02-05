f417801524fd388de24ff3ebfb75ba818382b583
Cesar Borges <cesar_silk321@hotmail.com>
2026-02-05 11:37:17 -0300

    electron added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection} from '@angular/core';
+import {provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling} from '@angular/router';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
-import { routes } from './app.routes';
+import {appRoutes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(
+      appRoutes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled'
+      }),
+      withEnabledBlockingInitialNavigation()
+    ),
+    provideHttpClient(withFetch()),
+    provideZonelessChangeDetection(),
+    providePrimeNG({theme: {preset: Aura, options: {darkModeSelector: '.app-dark'}}})
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import {Routes} from '@angular/router';
 import {Home} from './pages/home/home';
 
-export const routes: Routes = [
+export const appRoutes: Routes = [
   {
     path: '',
     component: Home,
