8c582c7b4faff0a329b73e5d77dce2ea4186a3ce
Cesar Borges <cesar_silk321@hotmail.com>
2026-02-05 15:15:32 -0300

    electron added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import {Routes} from '@angular/router';
-import {Home} from './pages/home/home';
+import {FullLayout} from '@/app/shared/layouts/full-layout/full-layout';
+import {BasicLayout} from '@/app/shared/layouts/basic-layout/basic-layout';
 
 export const appRoutes: Routes = [
   {
     path: '',
-    component: Home,
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: FullLayout,
+    loadChildren: () => import('./pages/pages-module').then(m => m.PagesModule),
+  },
+  {
+    path: '',
+    component: BasicLayout,
+    loadChildren: () => import('./auth/auth-module').then(m => m.AuthModule),
   }
 ];
